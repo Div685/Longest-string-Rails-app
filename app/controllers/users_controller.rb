6208dab18430e609301b77123a949120dabaf1ed
Div685 <divyeshpatel685@gmail.com>
2021-06-27 15:17:48 +0300

    Add methods to usercontroller
    - new, create, show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def index
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create!(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to "/users/#{@user.uname}", notice: 'User was Successfully created'  }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def show
+    @user = User.find_by(uname: params[:uname])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :name, :uname, :password)
   end
 end
