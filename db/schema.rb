8eeb2fba9cf3e8a352599a3ce9a09c367f8e68eb
Div685 <divyeshpatel685@gmail.com>
2021-06-28 13:22:49 +0300

    Add Algo model
    - to search names and algorithms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_144323) do
+ActiveRecord::Schema.define(version: 2021_06_28_084043) do
+
+  create_table "algos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
